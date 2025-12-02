209691b4d9ee2517a5086f0bc8750a393241f5cc
Rishu Raj <rishuraj@Rishus-MacBook-Air.local>
2025-12-02 20:19:37 +0530

    optimized fibonacci series

--- src/recursion/recursionSolutions.java
+++ src/recursion/recursionSolutions.java
@@ -10,12 +10,9 @@ public class recursionSolutions {
     }
 
     public int fibonacciSeries(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        }
-
+        if (n <= 1)
+            return n;
+        
         return fibonacciSeries(n-1) + fibonacciSeries(n-2);
     }
 }
