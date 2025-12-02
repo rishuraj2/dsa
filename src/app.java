4a7dc13ecd5594c47e0da917923ef6ad688d5548
Rishu Raj <rishuraj@Rishus-MacBook-Air.local>
2025-12-02 20:17:08 +0530

    added solution for fibonacciSeries to recursion/recursionSolutions.java

--- src/app.java
+++ src/app.java
@@ -3,7 +3,10 @@ import recursion.recursionSolutions;
 public class app {
     public static void main(String[] args) {
         recursionSolutions solutions = new recursionSolutions();
-        int result = solutions.factorial(10);
+        // int result = solutions.factorial(10); // output for factorial
+        // System.out.println(result); // output for factorial
+
+        int result = solutions.fibonacciSeries(15);
         System.out.println(result);
     }
 }
--- src/recursion/recursionSolutions.java
+++ src/recursion/recursionSolutions.java
@@ -8,4 +8,14 @@ public class recursionSolutions {
         }
         return n * factorial(n - 1); // recursive calls to factorial function 
     }
+
+    public int fibonacciSeries(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+
+        return fibonacciSeries(n-1) + fibonacciSeries(n-2);
+    }
 }
