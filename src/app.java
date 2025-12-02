68965030657a3670cc1315c4a294af8a0b1707e0
Rishu Raj <rishuraj@Rishus-MacBook-Air.local>
2025-12-02 19:54:19 +0530

    added solution for factorial to recursion/recursionSolutions.java

---
+++ src/app.java
@@ -0,0 +1,9 @@
+import recursion.recursionSolutions;
+
+public class app {
+    public static void main(String[] args) {
+        recursionSolutions solutions = new recursionSolutions();
+        int result = solutions.factorial(10);
+        System.out.println(result);
+    }
+}
