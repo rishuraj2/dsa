711470741265d096cf38da99e821fb53cab14ba2
Rishu Raj <rishuraj@Rishus-MacBook-Air.local>
2025-12-06 21:31:54 +0530

    updated ./src/Recursion and added solution for merge sort

--- src/app.java
+++ src/app.java
@@ -1,12 +1,12 @@
-import recursion.recursionSolutions;
+import java.util.Arrays;
 
 public class app {
     public static void main(String[] args) {
-        recursionSolutions solutions = new recursionSolutions();
-        // int result = solutions.factorial(10); // output for factorial
-        // System.out.println(result); // output for factorial
+        // System.out.println("Factorial of 10 = " + Factorial.factorial(10)); // output for factorial
+        // System.out.println("10th term in fibonacciSeries = " + FibonacciSeries.fibonacciSeries(10)); // output for fibonacciSeries
 
-        int result = solutions.fibonacciSeries(15);
-        System.out.println(result);
+        int[] arr = new int[]{5, 4, 3, 2, 6, 1, 8, 2};
+        Sorting.MergeSort.mergeSort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
 }
