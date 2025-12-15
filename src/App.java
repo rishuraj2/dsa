d5a4713498b338203dab6643ca8bcb4ee99f7713
Rishu Raj <rishuraj@Rishus-MacBook-Air.local>
2025-12-15 22:58:26 +0530

    added solution for searchInsert

--- src/App.java
+++ src/App.java
@@ -1,14 +1,12 @@
-
-import searching.LowerBound;
-
+import searching.SearchInsert;
 
 public class App {
     public static void main(String[] args) {
         // System.out.println("Factorial of 10 = " + Factorial.factorial(10)); // output for factorial
         // System.out.println("10th term in fibonacciSeries = " + FibonacciSeries.fibonacciSeries(10)); // output for fibonacciSeries
 
-        // int[] arr = new int[]{1, 1, 1, 1, 1, 2, 2, 3, 3, 3, 5, 5, 5, 6, 7, 8, 8, 8, 9, 9};
-        int[] arr = new int[]{};
+        int[] arr = new int[]{1, 3, 5, 6};
+        // int[] arr = new int[]{};
         // MergeSort.mergeSort(arr, 0, arr.length-1);
         // BubbleSort.bubbleSort(arr);
         // InsertionSort.insertionSort(arr);
@@ -16,6 +14,6 @@ public class App {
         // ReverseArray.reverseArray(arr, 0, arr.length-1);
         // System.out.println(Arrays.toString(arr)); // Output for sorting algorithms
 
-        System.out.println(LowerBound.lowerBound(arr, 0, arr.length-1, 20));
+        System.out.println(SearchInsert.searchInsert(arr, 5));
     }
 }
