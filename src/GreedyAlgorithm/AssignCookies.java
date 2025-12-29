ebb0cefd5b8e9e90a17ec22d82512064bd951771
Rishu Raj <sharmarishu695@gmail.com>
2025-12-30 01:57:47 +0530

    added solution for assignCookies

---
+++ src/GreedyAlgorithm/AssignCookies.java
@@ -0,0 +1,26 @@
+package GreedyAlgorithm;
+
+import java.util.Arrays;
+
+public class AssignCookies {
+    public static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int result = 0;
+
+        int i = 0;
+        int j = 0;
+
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                result++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return result;
+    }
+}
