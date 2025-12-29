3c0dd7ca551ba4f3d84ffa9aab2c953e1556c3f9
Rishu Raj <sharmarishu695@gmail.com>
2025-12-30 02:14:19 +0530

    added solution for JumpGame

---
+++ src/GreedyAlgorithm/JumpGame.java
@@ -0,0 +1,24 @@
+package GreedyAlgorithm;
+
+public class JumpGame {
+    public static boolean canJump(int[] nums) {
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] == 0) {
+                int stepsRequiredToSkip = 2;
+                boolean canBeSkipped = false;
+                for (int j = i-1; j >= 0; j--) {
+                    if (nums[j] >= stepsRequiredToSkip) {
+                        canBeSkipped = true;
+                        break;
+                    } else {
+                        stepsRequiredToSkip++;
+                    }
+                }
+                if (!canBeSkipped)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
