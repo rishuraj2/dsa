e0ad48b26ea3187683b07405275682853ddb52c7
Rishu Raj <sharmarishu695@gmail.com>
2025-12-30 13:24:54 +0530

    added solution for JumpGame2

---
+++ src/GreedyAlgorithm/JumpGame2.java
@@ -0,0 +1,17 @@
+package GreedyAlgorithm;
+
+public class JumpGame2 {
+    public static int jump(int[] nums) {
+        int minSteps = 0;
+        int i = nums.length - 1;
+         while (i > 0) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] >= i-j) {
+                    minSteps++;
+                    i = j;
+                }
+            }
+         }
+         return minSteps;
+    }                                                
+}
